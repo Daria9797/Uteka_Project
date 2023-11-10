96aafa13f33313f001b0d3ebd100ebe1da85f51a
Daria Kuteynikova <kolohmatowa@gmail.com>
2023-11-10 11:50:38 +0300

    add configuration remote

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "119");
         Configuration.browserSize = System.getProperty("size", "1920x1080");
-        //Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options",
                 Map.<String, Object> of(
