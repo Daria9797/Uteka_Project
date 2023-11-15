0478523fcb3c12394855cff229779d56a5550fa5
Daria9797 <144155239+Daria9797@users.noreply.github.com>
2023-11-15 12:08:23 +0300

    Update TestBase.java

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -19,6 +19,7 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("version", "100");
         Configuration.browserSize = System.getProperty("size", "1920x1080");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options",
                 Map.<String, Object> of(
